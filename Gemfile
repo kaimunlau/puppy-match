a5474f8ca7e1c2f480dfe5ac7b3a5b69062380a0
David Lau <david.kaimun.lau@gmail.com>
2023-11-20 15:41:38 +0100

    Add Devise and rename pages to pets.

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 
+# Use Devise for authentication
+gem "devise"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "puppy_match_production"
 
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'puppy-match-1415-1436c0b937ab.herokuapp.com/', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
