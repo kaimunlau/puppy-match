943609b3c84534aba3ec6072b54a8e36c689f19b
David Lau <david.kaimun.lau@gmail.com>
2023-11-21 14:13:55 +0100

    Design seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning db..."
+Pet.destroy_all
+User.destroy_all
+
+puts "Creating a user..."
+user = User.create!(
+  email: "test@test.com",
+  password: "123456",
+  first_name: "John",
+  last_name: "Doe",
+  address: "1 rue de la paix",
+  zipcode: "75000",
+  city: "Paris")
+puts "User created, email is #{user.email}, pw is #{user.password}!"
+
+puts "Creating pets..."
+2.times do
+  pet = Pet.create!(
+    name: Faker::Creature::Dog.name,
+    species: "Dog",
+    user_id: user.id,
+    price: rand(10..100))
+  puts "Pet created, name is #{pet.name}!"
+end
+
+puts "Done!"
