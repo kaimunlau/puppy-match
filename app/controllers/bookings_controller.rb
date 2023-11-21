80cb1c402f0b0d8a76d7d2a33ff7bd3389669014
laure Fontanie <fontanie.laure@hotmail.fr>
2023-11-21 14:40:04 +0100

    booking controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,27 @@
 class BookingsController < ApplicationController
+  before_action :set_pet, only: %i[new create]
+
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.pet = @pet
+    if @booking.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :pets_id, :user_id)
   end
 end
