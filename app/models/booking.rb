38ecda0573972b659027131ad0e6dd8c02857adb
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-21 13:23:03 +0100

    fix validations regarding start and end dates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,10 +2,27 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
 
-  enum status: %i[accepted pending denied]
+  enum status: { pending: 0, confirmed: 1, rejected: 2 }
 
-  validates :user_id, presence: true
-  validates :pet_id, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validate :start_date_not_in_past
+  validate :end_date_after_start_date
+
+  private
+
+  def start_date_not_in_past
+    return if end_date.blank? || start_date.blank?
+      if start_date <= Date.current
+        errors.add(:start_date, "must be after today")
+      end
+  end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+      if end_date < start_date
+        errors.add(:end_date, "must be after the start date")
+      end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_111027) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_114032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_111027) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.integer "status"
     t.bigint "user_id", null: false
     t.bigint "pet_id", null: false
     t.datetime "created_at", null: false
