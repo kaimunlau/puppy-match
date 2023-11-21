abffd603bcd72e6069a1b9b6fb833fb82b270ee2
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-21 11:58:41 +0100

    validations pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :users, through: :booking
   has_many_attached :photo
+
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :price, presence: true, comparison: { greater_than: 0 }
 end
