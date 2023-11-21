8b0a38655c3eb6778a1f8b6d8071026f2ec1acbc
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-21 10:22:48 +0100

    add action show && view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,14 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def show
+    pets = Pet.where(id: params[:id]) #collection // array
+    if !pets.empty?
+      @pet = pets[0]
+    else
+      flash.alert = "This pet doesn't exist!"
+      redirect_to :root
+    end
+  end
 end
