bdc8fb22e4a9fc58f32ae4242afd49153402d342
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-21 11:35:21 +0100

    add validations to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :pet
 
   enum status: %i[accepted pending denied]
+
+  validates :user_id, presence: true
+  validates :pet_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
