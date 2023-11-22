d0059dfa0e18384bf51e5c37ed5af4d6a19c8b71
David Lau <david.kaimun.lau@gmail.com>
2023-11-22 10:38:15 +0100

    Allow user to add new pet with photo.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,12 +4,32 @@ class PetsController < ApplicationController
   end
 
   def show
-    pets = Pet.where(id: params[:id]) #collection // array
-    if !pets.empty?
-      @pet = pets[0]
-    else
+    pets = Pet.where(id: params[:id]) # collection // array
+    if pets.empty?
       flash.alert = "This pet doesn't exist!"
       redirect_to :root
+    else
+      @pet = pets[0]
     end
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    pet.user = current_user
+    if pet.save
+      redirect_to pet_path(pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :price, :photo)
   end
 end
