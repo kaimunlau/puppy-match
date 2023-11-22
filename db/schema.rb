5bb4a2ab991a268c83672297ca27aed5c490d827
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-22 10:53:30 +0100

    finishing adding info on renter booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_114032) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_094350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_114032) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "user_id", null: false
     t.bigint "pet_id", null: false
     t.datetime "created_at", null: false
