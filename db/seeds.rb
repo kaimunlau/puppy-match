34d959ce2a53717f6c206a5e127f587563226bc1
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-22 09:04:16 +0100

    add seed renter

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Pet.destroy_all
 User.destroy_all
 
 puts "Creating a user..."
-user = User.create!(
+user1 = User.create!(
   email: "test@test.com",
   password: "123456",
   first_name: "John",
@@ -13,14 +13,24 @@ user = User.create!(
   address: "1 rue de la paix",
   zipcode: "75000",
   city: "Paris")
-puts "User created, email is #{user.email}, pw is #{user.password}!"
+puts "User created, email is #{user1.email}, pw is #{user1.password}!"
+
+user2 = User.create!(
+  email: "client@test.com",
+  password: "654321",
+  first_name: "Amy",
+  last_name: "Wheeler",
+  address: "175 Rue Saint-Denis",
+  zipcode: "75000",
+  city: "Paris")
+puts "Renter created, email is #{user2.email}, pw is #{user2.password}!"
 
 puts "Creating pets..."
 2.times do
   pet = Pet.create!(
     name: Faker::Creature::Dog.name,
     species: "Dog",
-    user_id: user.id,
+    user_id: user1.id,
     price: rand(10..100))
   puts "Pet created, name is #{pet.name}!"
 end
