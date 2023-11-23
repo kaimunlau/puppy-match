123039ee9a5d2b83ac7e1f4da92e2b76d3654dbe
David Lau <david.kaimun.lau@gmail.com>
2023-11-23 17:45:45 +0100

    Disable book button until dates are picked.

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="price"
 export default class extends Controller {
+  static targets = ["btn"]
+
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+    console.log(this.btnTarget)
+  }
+
+  handleDateChange(event) {
+    const dates = event.currentTarget.value
+    if (dates.includes("to")) {
+      this.btnTarget.classList.remove("disabled")
+    }
   }
 }
