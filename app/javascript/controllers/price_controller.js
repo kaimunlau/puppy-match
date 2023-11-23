c19661f314f0e7259b455a8900d05ada110a83ed
David Lau <david.kaimun.lau@gmail.com>
2023-11-23 17:53:16 +0100

    Display total price when dates are chosen.

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -2,15 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="price"
 export default class extends Controller {
-  static targets = ["btn"]
+  static targets = ["btn", "total"]
+
+  static values = {
+    price: Number
+  }
 
   connect() {
-    console.log(this.btnTarget)
   }
 
   handleDateChange(event) {
     const dates = event.currentTarget.value
     if (dates.includes("to")) {
+      const [start, end] = dates.split(" to ")
+      const days = (new Date(end) - new Date(start)) / 86400000
+      const total = days * this.priceValue
+      this.totalTarget.innerHTML = `Total: <strong>${total}€</strong>`
+
       this.btnTarget.classList.remove("disabled")
     }
   }
