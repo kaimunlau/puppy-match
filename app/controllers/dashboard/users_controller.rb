16dba3481db0e41ae68701921aec701e6d8d6f52
David Lau <david.kaimun.lau@gmail.com>
2023-11-23 12:53:11 +0100

    Display bookings for own pets.

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,6 +1,16 @@
 module Dashboard
   class UsersController < ApplicationController
     def home
+      pets_with_bookings = current_user.pets.select do |pet|
+        pet.bookings.any?
+      end
+      @bookings = pets_with_bookings.map do |pet|
+        {
+          pet: pet,
+          booking: pet.bookings.first,
+          total_price: pet.price * (pet.bookings.first.end_date - pet.bookings.first.start_date).to_i
+        }
+      end
     end
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :booking
   has_one_attached :photo # replace has_one_attached with has_many_attached if you want to upload multiple photos
 
