25857bd35f7fc7c60014cf8f1725725ecfd66dbd
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-24 10:30:26 +0100

    fix indentation

--- app/javascript/controllers/new_pet_controller.js
+++ app/javascript/controllers/new_pet_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="new-pet"
 export default class extends Controller {
   static targets = ["form"]
+
   connect() {
-    console.log("hello")
   }
 
   toggleForm() {
