01982e296c01fe4dba43c453394eccda82c51632
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-24 10:21:37 +0100

    form included in dashboard with toggle

--- app/javascript/controllers/new_pet_controller.js
+++ app/javascript/controllers/new_pet_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-pet"
 export default class extends Controller {
+  static targets = ["form"]
   connect() {
+    console.log("hello")
   }
+
+  toggleForm() {
+    this.formTarget.classList.toggle("d-none");
+  }
+
 }
