9d0a7929bae7648c933fdbf754a94fc88e909a57
laure Fontanie <fontanie.laure@hotmail.fr>
2023-11-24 12:30:33 +0100

    seeds enrichies

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,87 @@ owner3 = User.create!(
   city: "villeurbanne")
 puts "User created, email is #{owner3.email}, pw is #{owner3.password}!"
 
-owners = [owner1, owner2, owner3]
+owner4 = User.create!(
+  email: "owner3@test.com",
+  password: "123456",
+  first_name: "Jo",
+  last_name: "Bob",
+  address: "13 rue molière",
+  zipcode: "69006",
+  city: "Lyon")
+puts "User created, email is #{owner4.email}, pw is #{owner4.password}!"
+
+owner5 = User.create!(
+  email: "owner5@test.com",
+  password: "123456",
+  first_name: "marie",
+  last_name: "Curie",
+  address: "70 rue de créqui",
+  zipcode: "69006",
+  city: "Lyon")
+puts "User created, email is #{owner5.email}, pw is #{owner5.password}!"
+
+owner6 = User.create!(
+  email: "owner6@test.com",
+  password: "123456",
+  first_name: "Bernard",
+  last_name: "Morin",
+  address: "13 rue Garibaldi",
+  zipcode: "69006",
+  city: "Lyon")
+puts "User created, email is #{owner6.email}, pw is #{owner6.password}!"
+
+owner7 = User.create!(
+  email: "owner7@test.com",
+  password: "123456",
+  first_name: "Jean-Claude",
+  last_name: "Dusse",
+  address: "15 rue Royale",
+  zipcode: "69001",
+  city: "Lyon")
+puts "User created, email is #{owner7.email}, pw is #{owner7.password}!"
+
+owner8 = User.create!(
+  email: "owner8@test.com",
+  password: "123456",
+  first_name: "Jean-Claude",
+  last_name: "Dusse",
+  address: "11 rue d'Algérie",
+  zipcode: "69001",
+  city: "Lyon")
+puts "User created, email is #{owner8.email}, pw is #{owner8.password}!"
+
+owner9 = User.create!(
+  email: "owner9@test.com",
+  password: "123456",
+  first_name: "Chandler",
+  last_name: "Bing",
+  address: "4 rue de Belfort",
+  zipcode: "69004",
+  city: "Lyon")
+puts "User created, email is #{owner9.email}, pw is #{owner9.password}!"
+
+owner10 = User.create!(
+  email: "owner10@test.com",
+  password: "123456",
+  first_name: "Monica",
+  last_name: "Geller",
+  address: "11 Rue du Commandant Faurax",
+  zipcode: "69006",
+  city: "Lyon")
+puts "User created, email is #{owner10.email}, pw is #{owner10.password}!"
+
+owner11 = User.create!(
+  email: "owner11@test.com",
+  password: "123456",
+  first_name: "Rachel",
+  last_name: "Green",
+  address: "11 rue tronchet",
+  zipcode: "69006",
+  city: "Lyon")
+puts "User created, email is #{owner11.email}, pw is #{owner11.password}!"
+
+owners = [owner1, owner2, owner3, owner4, owner5, owner6, owner7, owner8, owner9, owner10, owner11]
 
 puts "Creating a renter..."
 client = User.create!(
@@ -49,22 +129,18 @@ client = User.create!(
 puts "Renter created, email is #{client.email}, pw is #{client.password}!"
 
 puts "Creating pets..."
-img_urls = [
-  "https://media.4-paws.org/1/e/d/6/1ed6da75afe37d82757142dc7c6633a532f53a7d/VIER%20PFOTEN_2019-03-15_001-2886x1999-1920x1330.jpg",
-  "https://www.petlandflorida.com/wp-content/uploads/2022/04/shutterstock_1290320698-1-scaled.jpg",
-  "https://images.ctfassets.net/sfnkq8lmu5d7/4Ma58uke8SXDQLWYefWiIt/3f1945422ea07ea6520c7aae39180101/2021-11-24_Singleton_Puppy_Syndrome_One_Puppy_Litter.jpg?w=1000&h=750&fl=progressive&q=70&fm=jpg",
-  "https://www.geniuspetfood.co.nz/cdn/shop/articles/Dog_food_for_puppies.jpg?v=1685817287",
-  "https://cdn.royalcanin-weshare-online.io/UCImMmgBaxEApS7LuQnZ/v2/eukanuba-market-image-puppy-beagle?w=5596&h=2317&rect=574,77,1850,1045&auto=compress,enhance"
+img_urls = ["https://source.unsplash.com/random/?puppy/orientation=landscape"
 ]
+
 20.times do
   file = URI.open(img_urls.sample)
   pet = Pet.new(
-    name: Faker::GreekPhilosophers.unique.name,
+    name: Faker::Artist.unique.name,
     species: "Dog",
     user_id: owners.sample.id,
     description: Faker::ChuckNorris.fact,
     taux_de_succes: rand(50..125.9),
-    price: rand(10..100))
+    price: rand(10..99))
   pet.photo.attach(io: file, filename: "puppy.jpg", content_type: "image/jpg")
   pet.save!
   puts "Pet created, name is #{pet.name}!"
